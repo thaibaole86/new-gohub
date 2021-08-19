007410b4765e59bf265dd99a1e2bfbdae2720295
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 09:42:04 +0000

    page.js: changed

--- src/templates/page.js
+++ src/templates/page.js
@@ -20,8 +20,8 @@ export default class Page extends React.Component {
     render() {
         return (
             <Layout {...this.props}>
-            <article className="page py-5 py-sm-6">
-            	<div className="container container--medium">
+            <article className="container">
+            	<div className="section">
             		<header className="page__header">
             			<h1 className="page__title">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
             		</header>
